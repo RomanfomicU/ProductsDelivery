0084a28ba6562c49baefbfdcbe3d89bf770d5e9d
Roman Fomichev <154666434+RomanfomicU@users.noreply.github.com>
2024-05-10 12:45:01 +0300

    Add main page

--- front/src/pages/MainPage.vue
+++ front/src/pages/MainPage.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        Main Page
+        <section class="service-description">
+            <h2>О нашей службе доставки</h2>
+            <p>ProDel - это надежная и быстрая служба доставки продуктов прямо к вашей двери. Мы стремимся обеспечить наших клиентов самыми качественными продуктами и отличным сервисом.</p>
+            <h3>Наши преимущества:</h3>
+            <ul>
+                <li>Точность и надежность: мы гарантируем точность и своевременность доставки.</li>
+                <li>Быстрая доставка: наши курьеры доставят ваш заказ в удобное для вас время.</li>
+                <li>Качество гарантировано: мы работаем только с проверенными поставщиками.</li>
+            </ul>
+        </section>
     </div>
 </template>
 
@@ -9,5 +18,25 @@
 </script>
 
 <style scoped>
+    .service-description {
+        max-width: 800px;
+        margin: 0 auto;
+    }
 
+    .service-description h2 {
+        color: #333;
+    }
+
+    .service-description h3 {
+        color: #555;
+    }
+
+    .service-description ul {
+        list-style-type: disc;
+        margin-left: 20px;
+    }
+
+    .service-description li {
+        margin-bottom: 10px;
+    }
 </style>
